e130c4bee5abc77129d5375e898dd79d3e8416d1
zRosies <gustavo.enrollment@gmail.com>
2023-10-10 11:44:41 -0300

    Atualizar .gitignore e limpar o cache do Git

--- routes/employees.js
+++
@@ -1,8 +0,0 @@
-const switcher = require('express').Router();
-const employees = require('../controllers/employees')
-
-
-switcher.get('/', employees.getAllEmployees)
-switcher.post('/', employees.createEmployee)
-
-module.exports= switcher;
--- routes/swagger.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const swaggerUI = require('swagger-ui-express')
-const swaggerJson = require('../swagger.json')
-
-router.use('/',swaggerUI.serve)
-router.get('/',swaggerUI.setup(swaggerJson))
-
-module.exports = router;
